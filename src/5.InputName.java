ad44c9ada45f33031e28517822fc08a74fd7daa7
Harsh Maurya <mauryaharsh963@gmail.com>
2026-08-07 23:09:08 +0530

    5th program

---
+++ src/5.InputName.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class InputName {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Good morning "+name);
+    }
+}
