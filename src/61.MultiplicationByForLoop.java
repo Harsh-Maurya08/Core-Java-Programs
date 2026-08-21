1127211f8a826aa573b9a9d025370d7c373ca367
Harsh Maurya <mauryaharsh963@gmail.com>
2026-08-21 20:06:13 +0530

    61st Program

---
+++ src/61.MultiplicationByForLoop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class MultiplicationByForLoop {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+        for(int i=1; i<=10;i++){
+            System.out.println(num*i);
+        }
+    }
+}
