f1f73cf2181a0117f0e74d11ac1f6be7c53ef66e
Harsh Maurya <mauryaharsh963@gmail.com>
2026-08-29 21:31:56 +0530

    96th Program

---
+++ src/Packages/Challenges/Challenge96/Enums/EnumTesting.java
@@ -0,0 +1,9 @@
+package Packages.Challenges.Challenge96.Enums;
+
+public class EnumTesting {
+    public static void main(String[] args) {
+        for (WeekDays days : WeekDays.values()) {
+            System.out.println(days+" ");
+        }
+    }
+}
---
+++ src/Packages/Challenges/Challenge96/Enums/WeekDays.java
@@ -0,0 +1,5 @@
+package Packages.Challenges.Challenge96.Enums;
+
+public enum WeekDays {
+    MONDAY, TUESDAY , WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY ;
+}
